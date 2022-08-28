05e93882ccc2d0f3d9c78a66c250fbb6e10b0cfb
Irzan Tri Saputra <36060059+Irzants@users.noreply.github.com>
2022-08-28 17:14:03 +0700

    UPDATE
    add some lines

--- courses_employees.sql
+++ courses_employees.sql
@@ -551,7 +551,46 @@ SELECT u.* FROM
         dept_manager m ON s.emp_no = m.emp_no;
 SELECT * FROM v_manager_avg_salary;
  
- 
+ /*DELIMITER*/
+DROP PROCEDURE IF EXISTS select_employees;
+
+DELIMITER $$
+CREATE PROCEDURE select_employees()
+BEGIN 
+	SELECT * FROM employees
+    LIMIT 1000;
+END$$
+DELIMITER ;
+
+call employees.select_employees();
+call select_employees(); /*if we already use database*/
+
+DELIMITER $$
+CREATE PROCEDURE avg_salary()
+BEGIN 
+	SELECT AVG(salary) FROM salaries;
+END$$
+DELIMITER ;
+CALL avg_salary();
+
+DROP PROCEDURE IF EXISTS avg_salary;
+
+/*STORED procedure with output parameter*/
+DELIMITER $$
+CREATE PROCEDURE emp_info(in p_first_name varchar(255), in p_last_name varchar(255), out p_emp_no integer)
+BEGIN
+                SELECT
+                                e.emp_no
+                INTO p_emp_no FROM
+                                employees e
+                WHERE
+                                e.first_name = p_first_name
+                                                AND e.last_name = p_last_name;
+
+END$$
+DELIMITER ;
+
+
  
  
  
