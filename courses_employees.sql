5340746ead1cb1c98c0add35cfe0104df0a4d2da
Irzan Tri Saputra <36060059+Irzants@users.noreply.github.com>
2022-08-29 14:44:28 +0700

    update lines
    add variable and user defined

--- courses_employees.sql
+++ courses_employees.sql
@@ -590,6 +590,57 @@ BEGIN
 END$$
 DELIMITER ;
 
+/*VARIABLES*/
+SET @v_emp_no = 0;
+
+CALL emp_info('Aruna', 'Journel', @v_emp_no);
+
+SELECT @v_emp_no; 
+ 
+/*USER-defined functions in MySql
+DELIMETER $$
+CREATE FUNVTION function_name(parameter data_type) RETURNS data_type
+DECLARE variable_name datat_type
+BEGIN 
+	SELECT. ...
+RETURN variable_name
+END $$
+DELIMITER;
+*/
+
+DELIMITER $$
+CREATE FUNCTION emp_info(p_first_name varchar(255), p_last_name varchar(255)) RETURNS decimal(10,2)
+
+DETERMINISTIC NO SQL READS SQL DATA
+
+BEGIN
+	DECLARE v_max_from_date date;
+    DECLARE v_salary decimal(10,2);
+SELECT 
+    MAX(from_date)
+INTO v_max_from_date FROM
+    employees e
+        JOIN
+    salaries s ON e.emp_no = s.emp_no
+WHERE
+    e.first_name = p_first_name
+        AND e.last_name = p_last_name;
+SELECT 
+    s.salary
+INTO v_salary FROM
+    employees e
+        JOIN
+    salaries s ON e.emp_no = s.emp_no
+WHERE
+    e.first_name = p_first_name
+        AND e.last_name = p_last_name
+        AND s.from_date = v_max_from_date;
+                RETURN v_salary;
+END$$
+DELIMITER ;
+
+SELECT EMP_INFO('Aruna', 'Journel');
+
 
  
  
